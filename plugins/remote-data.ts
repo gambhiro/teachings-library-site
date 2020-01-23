4a3c6c051c26b2533b1e79d79053573f5f30c58f
Gambhiro <gambhiro.bhikkhu.85@gmail.com>
2020-01-23 20:42:10 +0000

    upd

--- plugins/remote-data.ts
+++ plugins/remote-data.ts
@@ -1,8 +1,6 @@
 // Based on
 // https://steve.dignam.xyz/2018/12/27/sum-types-for-remote-data-in-typescript/
 
-import { HttpError } from 'http'
-
 // Response Kind
 export const enum rk {
   NotAsked,
@@ -12,27 +10,23 @@ export const enum rk {
 }
 
 interface NotAsked {
-  readonly kind: rk.NotAsked
+  readonly kind: rk.NotAsked;
 }
 
 interface Loading {
-  readonly kind: rk.Loading
+  readonly kind: rk.Loading;
 }
 
 interface Failure<E> {
-  readonly kind: rk.Failure
-  readonly data: E
+  readonly kind: rk.Failure;
+  readonly data: E;
 }
 
 interface Success<T> {
-  readonly kind: rk.Failure
-  readonly data: T
+  readonly kind: rk.Failure;
+  readonly data: T;
 }
 
-type RemoteData<E, T> =
-  | NotAsked
-  | Loading
-  | Failure<E>
-  | Success<T>
+type RemoteData<E, T> = NotAsked | Loading | Failure<E> | Success<T>;
 
-export type WebData<T> = RemoteData<HttpError, T>
+export type WebData<T> = RemoteData<{}, T>;
--- store/books.ts
+++ store/books.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { GetterTree, ActionTree, MutationTree } from 'vuex';
 import { RootState } from '~/store';
 import * as RD from '~/plugins/remote-data';
@@ -6,15 +8,15 @@ import { Book } from '~/types';
 type BooksData = {
   kind: RD.rk;
   data: Book[];
-  error: string;
+  error: any;
 };
 
 export type BooksStoreState = {
   all: BooksData;
 };
 
-export const state = (): BookStoreState => {
-  let s = {
+export const state = (): BooksStoreState => {
+  const s: BooksData = {
     kind: RD.rk.NotAsked,
     data: [],
     error: {}
@@ -23,40 +25,53 @@ export const state = (): BookStoreState => {
   return { all: s };
 };
 
-export const getters: GetterTree<BookStoreState, RootState> = {
+export const getters: GetterTree<BooksStoreState, RootState> = {
   allBooks: (state) => {
     return state.all;
   }
 };
 
-export const mutations: MutationTree<BookStoreState>  = {
-  setBooks(state, all: BooksData) {
+export const mutations: MutationTree<BooksStoreState> = {
+  setBooksAll(state, all: BooksData) {
     state.all = all;
+  },
+  setBooksSuccess(state, data: Array<Book>) {
+    state.all.kind = RD.rk.Success;
+    state.all.data = data;
+  },
+  setBooksFailure(state, e: any) {
+    state.all.kind = RD.rk.Failure;
+    state.all.error = e;
   }
 };
 
-export const actions: ActionTree<BookStoreState, RootState> = {
-  async fetchBooks ({ commit }) {
-    const all = await this.$api
-          .get('/books')
-          .then((res) => {
-            const s: BooksData = {
-              kind: RD.rk.Success,
-              data: res.data,
-              error: {}
-            };
-            return s;
-          })
-          .catch((e) => {
-            // context.error({ statusCode: 404, message: 'Books not found' })
+export const actions: ActionTree<BooksStoreState, RootState> = {
+  async fetchBooks({ commit }) {
+    const ret = await this.$api
+      .get('/books')
+      .then((res: any) => {
+        const s: BooksData = {
+          kind: RD.rk.Success,
+          data: res.data,
+          error: null
+        };
+        return s;
+      })
+      .catch((e: any) => {
+        // context.error({ statusCode: 404, message: 'Books not found' })
 
-            const s: BooksData = {
-              kind: RD.rk.Failure,
-              error: e
-            };
-            return s;
-          });
+        const s: BooksData = {
+          kind: RD.rk.Failure,
+          data: [],
+          error: e
+        };
+        return s;
+      });
 
-    commit('setBooks', all);
+    if (ret.kind === RD.rk.Success) {
+      commit('setBooksSuccess', ret);
+    } else {
+      commit('setBooksError', ret);
+    }
   }
 };
